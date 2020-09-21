d3b4a651882f2f039534b210eff1b40c7381c801
Valentina Gringot <glinelen@gmail.com>
2020-09-21 21:15:03 +0300

    add step location

--- src/index.js
+++ src/index.js
@@ -51,12 +51,13 @@ function Square(props) {
       this.state = {
         history:[{squares: Array(9).fill(null)}],
         xIsNext: true,
+        stepLocation: null,
         stepNumber: 0
       }
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if(calculateWinner(squares) || squares[i]) {
@@ -66,25 +67,30 @@ function Square(props) {
       this.setState({
         history: history.concat([{
           squares: squares,
-        }]),
+          stepLocation: i,
+        }]),        
+         stepNumber:history.length,
          xIsNext: !this.state.xIsNext
         });
     }
 
-    jumpTo(step) {
+    jumpTo(step, stepLocation) {
       this.setState({
-        stepNumber: step,
+        stepNumber: step,       
         xIsNext: (step % 2) === 0
       })
     }
 
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
-
+      const boardRowLength = 3;
+     
       const moves = history.map((step, move) => {
-        const desc = move ? 'Go to move #' + move : 'Go to game start';
+        const col = step.stepLocation % boardRowLength;
+        const row = Math.trunc(step.stepLocation / boardRowLength);
+        const desc = move ? 'Go to move #' + move + '( ' + col + ' , ' + row + ')'  : 'Go to game start';
         return (
           <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
